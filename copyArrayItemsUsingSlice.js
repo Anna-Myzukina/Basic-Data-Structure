951143a79ab18a3cb555194529319b3230c16754
Ann <anna.muzykina83@gmail.com>
2019-01-21 10:42:53 +0200

    Update copyArrayItemsUsingSlice.js

--- copyArrayItemsUsingSlice.js
+++ copyArrayItemsUsingSlice.js
@@ -16,3 +16,13 @@ In effect, we have created a new array by extracting elements from an existing a
 We have defined a function, forecast, that takes an array as an argument.
 Modify the function using slice() to extract information from the argument array 
 and return a new array that contains the elements 'warm' and 'sunny'.*/
+
+
+function forecast(arr) {
+  // change code below this line
+  
+  return arr.slice(2, 4);;
+}
+
+// do not change code below this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
