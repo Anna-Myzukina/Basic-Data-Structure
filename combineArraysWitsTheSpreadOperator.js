017f9e5047039093315eb0e2e26d3ead2af4e336
Ann <anna.muzykina83@gmail.com>
2019-01-21 10:58:48 +0200

    Update combineArraysWitsTheSpreadOperator.js

--- combineArraysWitsTheSpreadOperator.js
+++ combineArraysWitsTheSpreadOperator.js
@@ -15,3 +15,13 @@ more verbose had we used traditional methods.
 
 We have defined a function spreadOut that returns the variable sentence, modify the function using the spread operator
 so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].*/
+
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentense = ['learning', ...fragment, 'is', 'fun'];
+  
+  return sentence;
+}
+
+console.log(spreadOut);
