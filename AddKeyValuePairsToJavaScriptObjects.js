0a1b531726b837e340a549a0d3e05aa0a3bfc427
Ann <anna.muzykina83@gmail.com>
2019-01-26 21:39:56 +0200

    Update AddKeyValuePairsToJavaScriptObjects.js

--- AddKeyValuePairsToJavaScriptObjects.js
+++ AddKeyValuePairsToJavaScriptObjects.js
@@ -28,3 +28,20 @@ and a ReferenceError: followers is not defined would have been thrown.
 Using the same syntax, we can also add new key-value pairs to objects. 
 We've created a foods object with three entries. 
 Add three more entries: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.*/
+
+
+The foods object has already been declared. All that is left to be done is to add three new key-values.
+OBJECT[{KEY}] = {VALUE}
+The above code will create a ney key-value within the object.
+Solution
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+// change code below this line
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+// change code above this line
+console.log(foods);
