01cae1316e6444cd7a7768ea19ab5b3f0494c1fd
Ann <anna.muzykina83@gmail.com>
2019-01-26 21:07:22 +0200

    Update createComplexMultiDimentionalArrays.js

--- createComplexMultiDimentionalArrays.js
+++ createComplexMultiDimentionalArrays.js
@@ -47,5 +47,13 @@ Somewhere on the third level, include the string 'deep', on the fourth level, in
 and on the fifth level, include the string 'deepest'.*/
 
 
-
+let myNestedArray = [
+  // change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array',["deep"]],
+  ['mutate', 1327.98, 'splice', 'slice', 'push', [["deeper"]]],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[["deepest"]]] ]
+  // change code above this line
+];
 
