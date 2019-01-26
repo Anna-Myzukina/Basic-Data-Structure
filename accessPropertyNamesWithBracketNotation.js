fee74819c5b28b6826504bd5bf6bab07202ca4ab
Ann <anna.muzykina83@gmail.com>
2019-01-26 23:03:33 +0200

    Update accessPropertyNamesWithBracketNotation.js

--- accessPropertyNamesWithBracketNotation.js
+++ accessPropertyNamesWithBracketNotation.js
@@ -15,3 +15,31 @@ are not known before runtime or we need to access them in a more dynamic way.
 We've defined a function, checkInventory, which receives a scanned item as an argument. 
 Return the current value of the scannedItem key in the foods object. 
 You can assume that only valid keys will be provided as an argument to checkInventory.*/
+
+
+Example:
+let juice = {
+  apple: 1.15,
+  orange: 1.45
+};
+function checkInventory(scannedItem) {
+  return juice[scannedItem];
+}
+Solution:
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+// do not change code above this line
+
+function checkInventory(scannedItem) {
+  // change code below this line
+  return foods[scannedItem];
+}
+
+// change code below this line to test different cases:
+console.log(checkInventory("apples"));
