9ecbff346f89f95a941bd9c0f2513f0476136cb0
Ann <anna.muzykina83@gmail.com>
2019-02-02 14:03:06 +0200

    Update iterateThroughtTheKeyOfAnObjectWithAForInStatement.js

--- iterateThroughtTheKeyOfAnObjectWithAForInStatement.js
+++ iterateThroughtTheKeyOfAnObjectWithAForInStatement.js
@@ -25,3 +25,37 @@ is irrelevant when referencing or accessing that key.
 We've defined a function, countOnline; use a for...in statement within this function 
 to loop through the users in the users object and return the number of users whose 
 online property is set to true.*/
+
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function countOnline(obj) {
+  // change code below this line
+let online = 0;
+for (let name in obj) {
+  if(obj[name].online == true){
+    online += 1;
+  }
+}
+  return online;
+  // change code above this line
+}
+
+console.log(countOnline(users));
